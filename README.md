d786167be458931482bdbf590ba23e69687daabc
Jianwei Han <im.jianweihan@gmail.com>
2023-04-24 16:15:46 +0000

    Import ImgBB

--- README.md
+++ README.md
@@ -2,8 +2,12 @@ Image 2 Link
 ============
 发送图片给Telegram Bot, Telegram Bot会返回该图片的直链。
 
+## 背景
+看文档的时候经常会有截图的需求，但是整理图片相对于处理纯文字比较麻烦，本项目由此诞生。
+
 ## 示例
-![Example](https://api.telegram.org/file/bot5612837495:AAEOZCz6rgQbE6-fPorrMN1bM6EGP2saKuI/documents/file_3.png)
+![Example](https://i.ibb.co/Bz2Grz2/0060f5c85699.png)
+测试：https://t.me/images2link_bot
 
 ## 使用
-使用前请先设置环境变量`TOKEN`为您的Telegram Bot token
+使用前请先设置环境变量`TELEGRAM_BOT_TOKEN`为您的[Telegram Bot token](https://core.telegram.org/bots#creating-a-new-bot)，将环境变量`IMGBB_API_KEY`设置为您的[ImgBB api key](https://api.imgbb.com/)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, filters, CommandHandler, ContextTypes, MessageHandler
 import requests
+import urllib.request
+import uuid
+import base64
 
 
 # 设置日志等级
@@ -11,6 +14,12 @@ logging.basicConfig(level=logging.INFO,
 
 # 获取API Token
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# ImgBB API参数
+params = {
+    # 'expiration': '600',  # TODO: 引入文件一定时间后自动删除功能, 暂时为永久有效
+    "key": os.getenv("IMGBB_API_KEY"),
+}
 
 # 定义消息处理函数，接收图片并发送到Telegram Bot
 async def send_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -24,9 +33,33 @@ async def send_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     r = requests.get(f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={photo_file}")
     # 解析返回的JSON数据，获取图片直链
     file_path = r.json()['result']['file_path']
-    image_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
-    # 发送图片直链给用户
-    await context.bot.send_message(chat_id=chat_id, text=image_url)
+    telegram_image_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    file_name = f"./{str(uuid.uuid4())}{telegram_image_url[telegram_image_url.rfind('.'):]}"
+    # 这里为了不向ImgBB暴露我们的Telegram Bot token, 先将文件下载到本地再上传
+    urllib.request.urlretrieve(telegram_image_url, file_name)
+    with open(file_name, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        # 上传文件到ImgBB
+        files = {
+            "image": (None, encoded_string),
+        }
+
+        response = requests.post('https://api.imgbb.com/1/upload', params=params, files=files)
+
+        # 检查状态码
+        if response.status_code != 200:
+            await context.bot.send_message(chat_id=chat_id, text="抱歉, 处理失败, " + str(response.text))
+            return
+
+        data = response.json()
+        imgbb_image_url = data["data"]["image"]["url"]
+
+        # 发送图片直链给用户
+        await context.bot.send_message(chat_id=chat_id, text=f"Okay. {imgbb_image_url}")
+        
+    # 删除文件
+    os.remove(f"./{file_name}")
+   
 
 # 定义错误处理函数
 def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
